7743bd70b80e149fa4ffd357fc38200703cdd63c
ZarestiaDev <hishonoyoroi@gmail.com>
2022-05-15 14:44:55 +0200

    Deleted old comments

--- scripts/data_desktop_FFd20.lua
+++ scripts/data_desktop_FFd20.lua
@@ -6,14 +6,6 @@
 function onInit()
 	ModifierManager.addModWindowPresets(_tModifierWindowPresets);
 	ModifierManager.addKeyExclusionSets(_tModifierExclusionSets);
-
-	--[[
-	for k,v in pairs(_tDataModuleSets) do
-		for _,v2 in ipairs(v) do
-			Desktop.addDataModuleSet(k, v2);
-		end
-	end
-	]]--
 end
 
 -- Shown in Modifiers window
@@ -49,35 +41,3 @@ _tModifierExclusionSets =
 	{ "DEF_PCOVER", "DEF_COVER", "DEF_SCOVER" },
 	{ "DEF_CONC", "DEF_TCONC" },
 };
-
--- Shown in Campaign Setup window
-
---[[ Change later with names of the FFd20 modules
-_tDataModuleSets = 
-{
-	["client"] =
-	{
-		{
-			name = "3.5E - SRD",
-			modules =
-			{
-				{ name = "3.5E Basic Rules" },
-				{ name = "3.5E Spells" },
-			},
-		},
-	},
-	["host"] =
-	{
-		{
-			name = "3.5E - SRD",
-			modules =
-			{
-				{ name = "3.5E Basic Rules" },
-				{ name = "3.5E Magic Items" },
-				{ name = "3.5E Monsters" },
-				{ name = "3.5E Spells" },
-			},
-		},
-	},
-};
-]]--
