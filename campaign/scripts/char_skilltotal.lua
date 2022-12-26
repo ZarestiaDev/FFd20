33b81c0b28057ab0cc11b71a2f2232b9b8e74eca
ZarestiaDev <6247787+ZarestiaDev@users.noreply.github.com>
2022-12-26 17:51:33 +0100

    Update skill total calc

--- campaign/scripts/char_skilltotal.lua
+++ campaign/scripts/char_skilltotal.lua
@@ -1,24 +1,23 @@
 -- Function call and handler check whether skill ranks get adjusted
 function onInit()
-	CalculateSkills()
-	DB.addHandler(DB.getPath(getDatabaseNode(), "classes"), "onChildUpdate", CalculateSkills)
+	calculateSkills();
+	DB.addHandler(DB.getPath(getDatabaseNode(), "classes"), "onChildUpdate", calculateSkills);
 end
 
 -- Closing handler check whether skill ranks get adjusted
 function onClose()
-	DB.removeHandler(DB.getPath(getDatabaseNode(), "classes"), "onChildUpdate", CalculateSkills)
+	DB.removeHandler(DB.getPath(getDatabaseNode(), "classes"), "onChildUpdate", calculateSkills);
 end
 
 -- Add all class skills together
-function CalculateSkills()
-	local nodeChar = getDatabaseNode()
-	local tClasses = DB.getChildren(nodeChar.getChild("classes"))
-	local nTotalSkillRanks = 0
+function calculateSkills()
+	local nodeChar = getDatabaseNode();
+	local nTotalSkillRanks = 0;
 	
-	for _,v in pairs(tClasses) do
-		local nSkillRanks = DB.getValue(v, "skillranks", 0)
-		nTotalSkillRanks = nTotalSkillRanks + nSkillRanks
+	for _,vClass in pairs(DB.getChildren(nodeChar.getChild("classes"))) do
+		local nSkillRanks = DB.getValue(vClass, "skillranks", 0);
+		nTotalSkillRanks = nTotalSkillRanks + nSkillRanks;
 	end
 	
-	DB.setValue(nodeChar, "skillpoints.total", "number", nTotalSkillRanks)
+	DB.setValue(nodeChar, "skillpoints.total", "number", nTotalSkillRanks);
 end
